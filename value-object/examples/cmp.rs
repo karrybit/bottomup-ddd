b6f944c7d12d75120f43ffb81e70d1299b4603f0
karrybit <takumi.k.5610@gmail.com>
2021-07-21 23:03:57 +0900

    delete examples

--- value-object/examples/cmp.rs
+++
@@ -1,8 +0,0 @@
-use value_object;
-
-fn main() {
-    let name_a = value_object::FullName::new("masanobu", "naruse");
-    let name_b = value_object::FullName::new("john", "smith");
-    let compare_result = name_a == name_b;
-    println!("{}", compare_result);
-}

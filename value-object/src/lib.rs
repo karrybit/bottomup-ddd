058b5ccacb32fc14d8568b7c1daddd7f858b987f
karrybit <takumi.k.5610@gmail.com>
2021-07-21 23:04:08 +0900

    add FirstName, LastName

--- value-object/src/lib.rs
+++ value-object/src/lib.rs
@@ -1,24 +1,69 @@
-use std::collections::HashMap;
+use std::str::FromStr;
 
-#[derive(PartialEq, Eq, Hash)]
+#[derive(Debug, PartialEq, Eq, Hash)]
+pub struct FirstName(String);
+impl FromStr for FirstName {
+    type Err = String;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if s.is_empty() {
+            Err("1文字以上である必要があります".to_string())
+        } else {
+            Ok(FirstName(s.to_string()))
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Hash)]
+pub struct LastName(String);
+impl FromStr for LastName {
+    type Err = String;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if s.is_empty() {
+            Err("1文字以上である必要があります".to_string())
+        } else {
+            Ok(LastName(s.to_string()))
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Hash)]
 pub struct FullName {
-    first_name: String,
-    last_name: String,
+    first_name: FirstName,
+    last_name: LastName,
 }
 
 impl FullName {
-    pub fn new(first_name: &str, last_name: &str) -> FullName {
+    pub fn new(first_name: FirstName, last_name: LastName) -> FullName {
         FullName {
-            first_name: first_name.to_string(),
-            last_name: last_name.to_string(),
+            first_name,
+            last_name,
         }
     }
 
-    pub fn first_name(&self) -> &str {
+    pub fn first_name(&self) -> &FirstName {
         &self.first_name
     }
 
-    pub fn last_name(&self) -> &str {
+    pub fn last_name(&self) -> &LastName {
         &self.last_name
     }
+}
+
+#[test]
+fn test_equality_of_full_name() {
+    let name_a = FullName::new(
+        "masanobu".parse::<FirstName>().unwrap(),
+        "naruse".parse::<LastName>().unwrap(),
+    );
+    let name_b = FullName::new(
+        "john".parse::<FirstName>().unwrap(),
+        "smith".parse::<LastName>().unwrap(),
+    );
+    let name_c = FullName::new(
+        "masanobu".parse::<FirstName>().unwrap(),
+        "naruse".parse::<LastName>().unwrap(),
+    );
+
+    assert_ne!(name_a, name_b);
+    assert_eq!(name_a, name_c);
 }

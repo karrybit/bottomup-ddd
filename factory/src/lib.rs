c159b63ba9c62737bdc94b131eccd40fdaba391a
karrybit <takumi.k.5610@gmail.com>
2021-07-23 14:52:59 +0900

    update factory

--- factory/src/lib.rs
+++ factory/src/lib.rs
@@ -1,7 +1,45 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+struct UserID(i32);
+struct UserName(String);
+struct User {
+    id: UserID,
+    name: UserName,
+}
+
+trait UserFactory {
+    fn create(&self, name: UserName) -> User;
+}
+
+// InMemory(メモリ上でidを振る)やDBなど生成方法を抽象化
+// 抽象化の必要がないならFactoryの意味がない
+struct UserFactoryImpl {
+    current_id: i32,
+}
+
+impl UserFactory for UserFactoryImpl {
+    fn create(&self, name: UserName) -> User {
+        User {
+            id: UserID { 0: self.current_id },
+            name,
+        }
     }
+}
+
+struct UserApplicationService {
+    user_factory: Box<dyn UserFactory>,
+}
+
+impl UserApplicationService {
+    fn create(&self, name: UserName) -> User {
+        self.user_factory.create(name)
+    }
+}
+
+#[test]
+fn test_factory() {
+    let service = UserApplicationService {
+        user_factory: Box::new(UserFactoryImpl { current_id: 0 }),
+    };
+    let _ = service.create(UserName {
+        0: "name".to_string(),
+    });
 }
